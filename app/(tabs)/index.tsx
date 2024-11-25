7aad0489c897f726e015a2b0f57fed324b161677
Elias Khallouk <eliaskhallouk@gmail.com>
2024-11-25 00:37:08 -0500

    Home with logo and image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Bienvenue sur SportUniv !</Text>
+      <Image source={require('../logo2.png')} style={styles.imageLogo} />
+      <Text style={styles.title}>
+        Bienvenue sur <Text style={styles.highlight}>SportUniv</Text> !
+      </Text>
       <Text style={styles.subtitle}>Ton application de sport universitaire</Text>
+      <Image source={require('../Fitness.png')} style={styles.image} />
     </View>
   );
 }
@@ -18,49 +22,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#f0f0f0',
   },
   title: {
-    fontSize: 24,
+    fontSize: 40,
     fontWeight: 'bold',
-    color: '#333',
+    
+    marginBottom: 50,
+    textAlign: 'center',
+  },
+  highlight: {
+    color: '#5b7411',
   },
   subtitle: {
     fontSize: 18,
     color: '#666',
   },
+  image: {
+    width: '100%',
+    height: '50%',
+    marginTop: 20,
+  },
+  imageLogo: {
+    marginTop: 100,
+    width: 200,
+    height: 100
+  },
 });
-
-
-/*import React, { useEffect, useState } from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-import PageInfo from './screens/PageInfo';
-import HomeScreen from './screens/HomeScreen';
-
-const Stack = createStackNavigator();
-
-export default function App() {
-  const [isFirstLaunch, setIsFirstLaunch] = useState<boolean | null>(null);
-
-  useEffect(() => {
-    const checkFirstLaunch = async () => {
-      const hasLaunched = await AsyncStorage.getItem('hasLaunched');
-      setIsFirstLaunch(!hasLaunched);
-    };
-    checkFirstLaunch();
-  }, []);
-
-  if (isFirstLaunch === null) {
-    return null; // attendre que la vérification se termine
-  }
-
-  return (
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {isFirstLaunch ? (
-          <Stack.Screen name="Welcome" component={PageInfo} />
-        ) : (
-          <Stack.Screen name="Home" component={HomeScreen} />
-        )}
-      </Stack.Navigator>
-  );
-}
-*/
